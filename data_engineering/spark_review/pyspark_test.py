3db6b26ee59695170005eec2369a13e68c109ac9
robert-min <robertmin522@gmail.com>
2022-02-16 11:22:36 +0900

    22-02-16 pyspark_test.py Review

--- data_engineering/spark_review/pyspark_test.py
+++ data_engineering/spark_review/pyspark_test.py
@@ -1,1 +1,23 @@
+# 패키지
 import pandas as pd
+from pyspark import SparkConf, SparkContext
+
+# Spark 설정
+conf = SparkConf().setMaster("local").setAppName("pyspark_test")
+sc = SparkContext(conf=conf)
+
+# 데이터 로드
+directory = "/Users/robertmin/PycharmProjects/study/data_engineering/spark_review/data"
+filename = "tripdata_2020-03.csv"
+
+# 데이터 파싱
+lines = sc.textFile(f"file:///{directory}/{filename}")
+header = lines.first()
+filtered_lines = lines.filter(lambda row:row != header)
+
+# 원하는 데이터 추출
+dates = filtered_lines.map(lambda x: x.split(",")[3].split(" ")[0])
+result = dates.countByValue()
+
+# CSV로 결과 저장
+pd.Series(result, name='trips').to_csv("trip_data.csv")
