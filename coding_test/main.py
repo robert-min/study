8279aff85f1867f29c2556888e790e79e18c20d3
robert-min <robertmin522@gmail.com>
2022-05-05 16:11:56 +0900

    dp review

--- coding_test/main.py
+++ coding_test/main.py
@@ -1,61 +1,31 @@
-class Node:
-    def __init__(self, number, left_node, right_node):
-        self.parent = -1
-        self.number = number
-        self.left_node = left_node
-        self.right_node = right_node
-
-def in_order(node, level):
-    global level_depth, x
-    level_depth = max(level_depth, level)
-    if node.left_node != -1:
-        in_order(tree[node.left_node], level + 1)
-    level_min[level] = min(level_min[level], x)
-    level_max[level] = max(level_max[level], x)
-    x += 1
-    if node.right_node != -1:
-        in_order(tree[node.right_node], level + 1)
-
 n = int(input())
-tree = dict()
-level_min = [n]
-level_max = [0]
-root = -1
-x = 1
-level_depth = 1
+array = list()
+array.append((0, 0,0, 0))
 
-# 트리 생성
 for i in range(1, n + 1):
-    tree[i] = Node(i, -1, -1)
-    level_min.append(n)
-    level_max.append(0)
+    area, height, weight = map(int, input().split())
+    array.append((i, area, height, weight))
 
-# tree 값 입력
-for _ in range(n):
-    number, left_node, right_node = map(int, input().split())
-    tree[number].left_node = left_node
-    tree[number].right_node = right_node
-    # 노드 연결
-    if left_node != -1:
-        tree[left_node].parent = number
-    if right_node != -1:
-        tree[right_node].parent = number
+# 무게 기준으로 정렬
+array.sort(key=lambda x : x[3])
 
-# 최상단 노드 찾음
+# 높이 저장
+dp = [0] * (n + 1)
 for i in range(1, n + 1):
-    if tree[i].parent == -1:
-        root = i
+    for j in range(0, i):
+        if array[i][1] > array[j][1]:
+            dp[i] = max(dp[i], dp[j] + array[i][2])
 
-# 각 레벨 별 가장 왼쪽 값(level_min)과 오른쪽 값(level_max)을 찾아서 입력
-in_order(tree[root], 1)
+# 역추적
+max_value = max(dp)
+index = n
+result = list()
+while index != 0:
+    if max_value == dp[index]:
+        result.append(array[index][0])
+        max_value -= array[index][2]
+    index -= 1
 
-# 결과 출력
-result_level = 1
-result_width = level_max[1] - level_min[1] + 1
-for i in range(2, level_depth + 1):
-    width = level_max[i] - level_min[i] + 1
-    if result_width < width:
-        result_level = i
-        result_width = width
-
-print(result_level, result_width)
+result.reverse()
+print(len(result))
+[print(i) for i in result]
