cad3e1e9c5f249b6cdef5ce4ac7e818881309d93
robert-min <robertmin522@gmail.com>
2022-05-06 19:56:49 +0900

    kakao 2021 question ~3 study

--- coding_test/main.py
+++ coding_test/main.py
@@ -1,53 +1,69 @@
-# 대기실마다 확인하면서 "P"인 곳에서 bfs 호출
-# bfs 함수에서 좌표 및 거리를 큐에 넣음
-## 거리가 2 미만이고 다음 방문할 곳이 "O"라면 큐에 넣음
-## 거리가 2 미만이고 다음 방문할 곳이 "P"라면 False
-## 거리가 2면 중지
 from collections import deque
+# 노드 생성
+class Node:
+    def __init__(self):
+        self.prev = -1
+        self.next = -1
+        self.is_delete = False  # 삭제 여부 판단
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-def bfs(array, x, y):
-    visited = [[False] * 5 for _ in range(5)]
-    q = deque()
-    q.append((x, y, 0))
-    visited[x][y] = True
-    while q:
-        x, y, cost = q.popleft()
-        if cost == 2:
-            continue
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < 5 and 0 <= ny < 5:
-                if visited[nx][ny]:
-                    continue
-                if array[nx][ny] == "P":
-                    return False
-                if array[nx][ny] == "O":
-                    q.append((nx, ny, cost + 1))
-                    visited[nx][ny] = True
-    return True
+def solution(n, k, cmd):
+    # 링크드 리스트 초기화
+    node_list = [Node() for _ in range(n)]
+    for i in range(n - 1):
+        node_list[i].next = i + 1
+        node_list[i + 1].prev = i
 
-def solution(places):
-    answer = []
+    # 삭제된 노드 저장할 스택
+    del_stack = deque()
 
-    for place in places:
-        array = [list(place[i]) for i in range(5)]
-        flag = True
+    # 명령어 처리
+    cur = k
+    for c in cmd:
+        if len(c) > 1:
+            c, move_size = c.split(" ")
+            move_size = int(move_size)
 
-        for i in range(5):
-            for j in range(5):
-                if array[i][j] == "P":
-                    if not bfs(array, i, j):
-                        flag = False
-            if not flag:
-                break
+        # U
+        if c == "U":
+            for i in range(move_size):
+                cur = node_list[cur].prev
 
-        if flag:
-            answer.append(1)
+        # D
+        if c == "D":
+            for i in range(move_size):
+                cur = node_list[cur].next
+
+        # C
+        if c == "C":
+            node_list[cur].is_delete = True
+            del_stack.append(cur)
+            prev_node = node_list[cur].prev
+            next_node = node_list[cur].next
+
+            if prev_node != -1:
+                node_list[prev_node].next = next_node
+            if next_node != -1:
+                node_list[next_node].prev = prev_node
+            else:
+                cur = prev_node
+
+        # Z
+        if c == "Z":
+            del_node = del_stack.pop()
+            node_list[del_node].is_delete = False
+            prev_node = node_list[del_node].prev
+            next_node = node_list[del_node].next
+
+            if prev_node != -1:
+                node_list[prev_node].next = del_node
+            if next_node != -1:
+                node_list[next_node].prev = del_node
+
+    # 출력
+    answer = ''
+    for i in range(n):
+        if node_list[i].is_delete:
+            answer = answer + "X"
         else:
-            answer.append(0)
-
+            answer = answer + "O"
     return answer
-
