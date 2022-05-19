6e0f18226ce26f87ceaddac9f23460aa437d928e
robert-min <robertmin522@gmail.com>
2022-05-20 00:23:09 +0900

    dp study

--- coding_test/main.py
+++ coding_test/main.py
@@ -1,35 +1,14 @@
-from copy import deepcopy
+import copy
 
-# 입력
 N = int(input())
-Board = [list(map(int, input().split())) for i in range(N)]
+array = list(map(int, input().split()))
 
-def rotate90(B, N):
-    NB = deepcopy(B)
-    for i in range(N):
-        for j in range(N):
-            NB[j][N-i-1] = B[i][j]  # 90도 회전
-    return NB
+# dp[i] : i까지 왔을 때 합이 최대
+dp = copy.deepcopy(array)
 
-def convert(lst, N):
-    new_list = [i for i in lst if i]
-    for i in range(1, len(new_list)):
-        if new_list[i-1] == new_list[i]:
-            new_list[i -1] *= 2
-            new_list[i] = 0
-    new_list = [i for i in new_list if i]
-    return new_list + [0] * (N-len(new_list))
+for i in range(1, N):
+    for j in range(i):
+        if array[i] > array[j]:
+            dp[i] = max(array[i] + dp[j], dp[i])
 
-def dfs(N, B, count):
-    ret = max([max(i) for i in B])
-    if count == 0:
-        return ret
-    for _ in range(4):
-        X = [convert(i, N) for i in B]
-        if X != B:
-            ret = max(ret, dfs(N, X, count - 1))
-        B = rotate90(B, N)
-    return ret
-
-
-print(dfs(N, Board, 5))
+print(max(dp))
